c42dfdc70030cd4463af734bb68181f8177f2f96
M_goncalves <michaelagoncalves13082000@gmail.com>
2026-01-09 11:39:06 +0200

    progress

--- Cozy-Code/script.js
+++ Cozy-Code/script.js
@@ -3,6 +3,12 @@ const clickText = document.getElementById('begin');
 const welcome = document.getElementById('welcome');
 const muteBtn = document.getElementById('muteBtn');
 const volumeSlider = document.getElementById('volumeSlider');
+const avatarScreen = document.getElementById('avatarScreen');
+
+//Volume slider controls
+volumeSlider.addEventListener('input', (e) => {
+  bgMusic.volume = e.target.value;
+})
 
 //Toggle mute/unmute and change button image
 muteBtn.addEventListener('click', () => {
@@ -12,21 +18,47 @@ muteBtn.addEventListener('click', () => {
     : "url('assets/high-volume.png')";
 });
 
-//Volume slider controls
-volumeSlider.addEventListener('input', (e) => {
-  bgMusic.volume = e.target.value;
-})
-
+//Step 1: When "Click to Begin" finishes typing
 clickText.addEventListener('animationend', () => {
   clickText.classList.add('typing-done');
 }, {once: true});
 
+//Step 2: User clicks "Click to Begin"
 clickText.addEventListener('click', () => {
+  //Hide "Click to Begin"
   clickText.style.display = 'none';
+  //Start music
   bgMusic.play();
-  welcome.classList.add('typing');
-  welcome.addEventListener('animationend', () => {
-  welcome.classList.add('typing-done');
-}, {once: true});
-})
+  //Start Welcome typing
+  welcome.classList.add('typing2');
+  //Step 3: When welcome finishes typing
+});
 
+  welcome.addEventListener('animationend', () => {
+    //Hide Welcome text
+  setTimeout(() => {
+    welcome.style.display = 'none';
+      avatarScreen.classList.remove('hidden');
+    }, 1500); 
+  }, {once : true});
+
+  //Avatar elements
+  const boyAvatar = document.getElementById('boy-avatar');
+  const girlAvatar= document.getElementById('girl-avatar');
+
+  //When user clicks boy avatar
+  boyAvatar.addEventListener('click', () => {
+    localStorage.setItem('chosenAvatar', 'boy');
+    startGame() // to move to the next screen/phase
+  });
+
+  //when user clicks girl avatar
+  girlAvatar.addEventListener('click', () => {
+    localStorage.setItem('chosenAvatar', 'girl');
+    startGame() 
+  });
+
+  function startGame() {
+    avatarScreen.style.display = 'none';
+    console.log('Avatar chosen:', localStorage.getItem('chosenAvatar'));
+  }
